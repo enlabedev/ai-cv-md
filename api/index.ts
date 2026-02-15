20bac67e189ecceeef16b38cadc1df0ad244c3bc
Enrique Lazo Bello <enlabe@gmail.com>
2026-02-15 12:07:14 -0500

    refactor: simplify handler and remove serverless-express integration

--- api/index.ts
+++ api/index.ts
@@ -1,27 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { configureApp } from '../src/setup';
-import serverlessExpress from '@codegenie/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
 
-let server: Handler;
+let app: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   configureApp(app);
   await app.init();
-
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+  return app.getHttpAdapter().getInstance();
 }
 
-export default async function handler(
-  event: any,
-  context: Context,
-  callback: Callback,
-) {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
+export default async function handler(req: any, res: any) {
+  if (!app) {
+    app = await bootstrap();
+  }
+  return app(req, res);
 }
 
 export const config = {
